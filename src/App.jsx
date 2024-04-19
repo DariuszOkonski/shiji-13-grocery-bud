6dc3ac89e42fe35cf05235caa9eee00dbf4f63a5
Dariusz Okoński <darek200180@gmail.com>
2024-04-19 19:09:24 +0200

    add to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,24 @@ import { nanoid } from 'nanoid';
 import { ToastContainer, toast } from 'react-toastify';
 import Items from './Items';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = [];
+  }
+  return list;
+
+  //or const default = JSON.parse(localStorage.get('list') || '[]')
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
 
   const addItem = (itemName) => {
     const newItem = {
@@ -15,7 +31,9 @@ const App = () => {
       id: nanoid(),
     };
 
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
 
     toast.success('Item added');
   };
@@ -23,6 +41,8 @@ const App = () => {
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+
+    setLocalStorage(newItems);
     toast.success('Item removed');
   };
 
